d6e45516962d4dec43f8424769149d78d0261b1d
larissa beatriz costa <larissa@grupovex.com.br>
2025-05-07 16:26:24 -0300

    feat: :sparkles: app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageBackgroundComponent } from "./components/image-background/image-background.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ImageBackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/image-background/image-background.component.ts
@@ -0,0 +1,12 @@
+import { NgIf } from '@angular/common';
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-image-background',
+  templateUrl: './image-background.component.html',
+  styleUrls: ['./image-background.component.css'],
+  imports: [NgIf],
+})
+export class ImageBackgroundComponent {
+ @Input() showLogo:boolean = true;
+}
